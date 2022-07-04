da858feec116f38fee84fa13dba26eb7bc47a456
jgbpessoa <jgbpessoa@gmail.com>
2022-07-04 19:50:43 -0300

    Working with State and Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./models/todo";
 
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
     console.log(text);
+    // setTodos([...todos, { id: Math.random().toString(), text }]);
+    // This does not guarantee the latest state snapshot -> theoretically react schedules state updates therefore todos value here might not necessarily be the latest state when this update is performed
+    // Passing an arrow function with the previous state as a parameter is the best practice
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text },
+    ]);
   };
 
   return (
